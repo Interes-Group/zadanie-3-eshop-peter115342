891a00e393bef43f6be08198340de6bb26d28470
peter115342 <pmuzslay8@gmail.com>
2023-05-12 16:39:49 +0200

    Fixes.

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/data/IProductRepository.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/data/IProductRepository.java
@@ -3,7 +3,6 @@ package sk.stuba.fei.uim.oop.assignment3.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface IProductRepository extends JpaRepository<Product, Long> {
